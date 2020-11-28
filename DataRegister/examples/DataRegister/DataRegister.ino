a136b7f994d204685ddba53891da37746c930a4f
Ruben <ronatvik@gmail.com>
2020-11-28 12:36:38 +0100

    Converted to library structure

--- DataRegister/examples/DataRegister/DataRegister.ino
+++ DataRegister/examples/DataRegister/DataRegister.ino
@@ -1,9 +1,10 @@
-#include "DataRegister.h"
+#include <DataRegister.h>
 
 float * my_float;
 int * my_int;
 word * my_word;
 
+// Obligatory variables for DataRegister
 const int num_registers = 3;
 const int buffer_size = 4 + 2 + 2; // float + int + word
 int size_array[] = {4, 2, 2};
@@ -12,13 +13,13 @@ byte value_buffer[buffer_size];
 
 void setup() {
   Serial.begin(115200);
-  
+  // Declare a DataRegister
   DataRegister data_register = DataRegister(buffer_size, num_registers, value_buffer, size_array, index_array);
   
   my_float = (float*) data_register.link(0); *my_float = -24.23;
   my_int   = (int*)   data_register.link(1); *my_int  = 1337;
   my_word  = (word*)  data_register.link(2); *my_word  = 0x8008;
-
+  
   Serial.println(1); Serial.print(*my_float); Serial.print(", "); Serial.print(*my_int); Serial.print(", "); Serial.println(*my_word);
   float new_float = 12.34;
   int new_int = 8008;
